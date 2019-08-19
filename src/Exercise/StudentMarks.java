83409add6a46c9b1f0e3ca8261c6d35a0bc3f9c9
nadinn <nadin.ja@hotmail.com>
2019-08-19 23:06:25 +0100

    1

---
+++ src/Exercise/StudentMarks.java
@@ -0,0 +1,44 @@
+package Exercise;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class StudentMarks {
+    public double[] marks = new double[7];
+    Random rn = new Random();
+    DecimalFormat df = new DecimalFormat("#.##");
+
+    // constructor populates automatically the array with random marks when class is initialised
+    public StudentMarks(){
+        for(int i = 0; i<this.marks.length; i++){
+            this.marks[i]=rn.nextDouble()*100;
+        }
+    }
+    // printing out the marks individually
+    public void getMarks(){
+        System.out.println("The marks are: ");
+        for ( int i = 0; i<this.marks.length ; i++){
+            System.out.print( df.format(this.marks[i]) + "  " );
+        }
+    }
+
+     // getting a specific marks at a position specified
+     public double getSpecificMark(int pos){
+        return this.marks[pos];
+     }
+     //setting a specified mark at a specified position
+     public void setMark(int pos, double mark){
+        this.marks[pos]=mark;
+     }
+     // returns the mean of all the marks
+     public double meanMarks(){
+        double mean = 0;
+        double sum = 0;
+        for (int i =0; i<this.marks.length; i++){
+            sum = sum+this.marks[i];
+        }
+        mean= sum/this.marks.length;
+        return mean;
+     }
+
+}
---
+++ src/Exercise/StudentMarksTest.java
@@ -0,0 +1,24 @@
+package Exercise;
+
+import java.text.DecimalFormat;
+
+public class StudentMarksTest {
+    public static void main (String args []){
+        DecimalFormat df = new DecimalFormat("#.##");
+        StudentMarks st = new StudentMarks();
+        int pos = 3;
+
+        st.getMarks(); // printing all the marks
+        System.out.println();
+
+        System.out.println("The mark at position "+ pos+ " is: "+ df.format(st.getSpecificMark(pos))); // getting a marks
+        st.setMark(2, 80);  // setting a new mark at a specified position
+
+        System.out.println("The new marks are: ");
+        st.getMarks(); // testing the new mark position
+        System.out.println();
+        System.out.println("The mean of the marks is: " + df.format(st.meanMarks()));
+
+
+    }
+}
